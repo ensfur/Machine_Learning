67ec0df6fcff799ea2c7eb3e425fceeb6c04200d
Furkan COZELI <84848844+ensfur@users.noreply.github.com>
2023-05-26 21:44:17 +0300

    Update logistic_Regression.py

--- logistic_Regression.py
+++ logistic_Regression.py
@@ -13,7 +13,6 @@ from sklearn.metrics import confusion_matrix
 sc = StandardScaler()
 lr = LogisticRegression()
 
-
 data = pd.read_csv("urun.csv")
 x = data.iloc[:,0:2].values
 y = data.iloc[:,-1].values.reshape(-1,1)
@@ -23,11 +22,9 @@ xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3)
 S = data[data.iloc[:,2]==0]
 B = data[data.iloc[:,2]==1]
 
-
 plt.figure()
 plt.scatter(S.iloc[:,0].values, S.iloc[:,1].values, color="red")
 plt.scatter(B.iloc[:,0].values, B.iloc[:,1].values, color="blue")
-
 
 xtrain1 = sc.fit_transform(xtrain)
 xtest1 = sc.fit_transform(xtest)
@@ -39,18 +36,7 @@ yHead = lr.predict(xtest1)
 print(lr.score(xtest1,ytest))
 
 cm = confusion_matrix(ytest, yHead)
-# [[68  3]      68 doğru 3 yanlış   satın almama durumu için
-# [17 32]]      32 doğru 17 yanlış  satın alma durumu için
 print(cm)
 
-
-
-
-
-
-
-
-
 plt.show()
-
 
