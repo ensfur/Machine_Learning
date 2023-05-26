9f9790d2aa383aa445acb95fb2eee689e6e62bef
Furkan COZELI <84848844+ensfur@users.noreply.github.com>
2023-05-26 21:44:49 +0300

    Update multi_Linear_Regression.py

--- multi_Linear_Regression.py
+++ multi_Linear_Regression.py
@@ -1,11 +1,8 @@
 # Multi Linear Regression
-# Birden fazla sütunun sonuç üzerindeki etkisini gözlemlemek için kullanılır.
-# f(x,y,z) = t
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
 
 data = pd.read_csv("reklam.csv")
 lr = LinearRegression()
@@ -18,7 +15,4 @@ xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3)
 lr.fit(x, y)
 
 print(lr.predict(xtest))
-
-
-
 
